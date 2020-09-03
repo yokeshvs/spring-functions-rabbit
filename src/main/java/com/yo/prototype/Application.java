ed80de2047194fe5522d019711d129f4ecb37685
Yokesh <yokeshvsk@gmail.com>
2020-09-02 20:39:49 -0500

    Initial commit

--- src/main/java/com/yo/prototype/Application.java
+++ src/main/java/com/yo/prototype/Application.java
@@ -3,7 +3,6 @@ package com.yo.prototype;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import reactor.core.publisher.Flux;
 
 import java.util.function.Function;
 
